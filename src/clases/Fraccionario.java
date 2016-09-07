db4c9ad85eab64b37ea7c1b8c031c0d509b15344
jparedes17 <paredesjorge10@hotmail.com>
2016-09-07 18:12:20 -0500

    Ejercicio casi terminado

--- src/clases/Fraccionario.java
+++ src/clases/Fraccionario.java
@@ -47,5 +47,100 @@ public class Fraccionario {
     public void setMixto(int mixto) {
         this.mixto = mixto;
     }
+    
+    public Fraccionario convertir (Fraccionario f2)
+    {
+        int num, den, mix;
+        Fraccionario f;
+        mix= this.mixto;
+        num=(mix*this.denominador)+this.numerador;
+        den= this.denominador;
+        f = new Fraccionario (num, den, mix); 
+        return f;
+    }
+    public Fraccionario convertir2 (Fraccionario f2)
+    {
+        int num, den, mix;
+        Fraccionario f;
+        
+        mix=f2.mixto;
+        num= (mix*f2.denominador)+f2.numerador;
+        den= f2.denominador;
+        f = new Fraccionario (num, den, mix);
+        return f;
+    }
+    
+    public Fraccionario suma (Fraccionario f2)
+    {
+        int num, den, mix;
+        Fraccionario f;
+        
+        num= (this.numerador*f2.denominador)+(this.denominador*f2.numerador);
+        den= this.denominador*f2.denominador;
+        f = new Fraccionario (num, den, 1);
+        return f;
+        
+    }
+    
+    public Fraccionario resta (Fraccionario f3)
+    {
+        int num, den, mix;
+        Fraccionario f;
+        
+        num= (this.numerador*f3.denominador)-(this.denominador*f3.numerador);
+        den= this.denominador*f3.denominador;
+        f = new Fraccionario (num, den, 1);
+        return f;
+        
+    }
+    public Fraccionario multiplicacion (Fraccionario f3)
+    {
+        int num, den, mix;
+        Fraccionario f;
+        
+        num= this.numerador*f3.numerador;
+        den= this.denominador*f3.denominador;
+        f = new Fraccionario (num, den, 1);
+        return f;
+        
+    }
+    
+    public Fraccionario division (Fraccionario f3)
+    {
+        int num, den, mix;
+        Fraccionario f;
+        
+        num= this.numerador*f3.denominador;
+        den= this.denominador*f3.numerador;
+        f = new Fraccionario (num, den, 1);
+        return f;
+        
+    }
+    
+    public Fraccionario convertir3 (Fraccionario f7)
+    {
+        int num, den, mix;
+        Fraccionario f;
+        
+      num= f7.numerador%f7.denominador;
+      den= f7.denominador;
+      mix= f7.numerador/f7.denominador;
+    
+    f= new Fraccionario (num, den, mix);
+    return f;
+        
+    }
+    
+    public Fraccionario convertir4 (Fraccionario f3)
+    {
+       int num, den, mix;
+       Fraccionario f;
+       
+       mix= (f3.mixto*f3.denominador)+f3.numerador;
+       num= mix;
+       den= f3.denominador;
+       f = new Fraccionario (num, den, mix);
+       return f;
+    }
 }
 
--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -5,6 +5,8 @@
  */
 package interfaz;
 
+import clases.Fraccionario;
+
 /**
  *
  * @author pared
@@ -27,21 +29,146 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        txtNumerador1 = new javax.swing.JTextField();
+        txtDenominador1 = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        txtMixto1 = new javax.swing.JTextField();
+        txtNumerador2 = new javax.swing.JTextField();
+        txtDenominador2 = new javax.swing.JTextField();
+        jSeparator2 = new javax.swing.JSeparator();
+        txtMixto2 = new javax.swing.JTextField();
+        txtNumerador3 = new javax.swing.JTextField();
+        txtDenominador3 = new javax.swing.JTextField();
+        jSeparator3 = new javax.swing.JSeparator();
+        txtMixto3 = new javax.swing.JTextField();
+        cmbOperaciones = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtNumerador4 = new javax.swing.JTextField();
+        jSeparator4 = new javax.swing.JSeparator();
+        txtDenominador4 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("OPERACIONES CON FRACCIONES");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 20, -1, -1));
+        jPanel1.add(txtNumerador1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 60, 60, -1));
+        jPanel1.add(txtDenominador1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 100, 60, -1));
+
+        jSeparator1.setBackground(new java.awt.Color(0, 0, 0));
+        jSeparator1.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 90, 80, 10));
+        jPanel1.add(txtMixto1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, 40, 40));
+        jPanel1.add(txtNumerador2, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 60, 60, -1));
+        jPanel1.add(txtDenominador2, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 100, 60, -1));
+
+        jSeparator2.setBackground(new java.awt.Color(0, 0, 0));
+        jSeparator2.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 90, 80, 10));
+        jPanel1.add(txtMixto2, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 70, 40, 40));
+        jPanel1.add(txtNumerador3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 60, 60, -1));
+        jPanel1.add(txtDenominador3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 100, 60, -1));
+
+        jSeparator3.setBackground(new java.awt.Color(0, 0, 0));
+        jSeparator3.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 90, 80, 10));
+        jPanel1.add(txtMixto3, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 70, 40, 40));
+
+        cmbOperaciones.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Suma", "Resta", "Multiplicacion", "Division" }));
+        jPanel1.add(cmbOperaciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 80, 60, -1));
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel2.setText("=");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 80, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setText("FRACCION ");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, -1, -1));
+        jPanel1.add(txtNumerador4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 190, 60, -1));
+
+        jSeparator4.setBackground(new java.awt.Color(0, 0, 0));
+        jSeparator4.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel1.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 220, 80, 10));
+        jPanel1.add(txtDenominador4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 230, 60, -1));
+
+        jButton1.setText("Calcular");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 170, -1, -1));
+
+        jButton2.setText("Borrar");
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 170, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 400));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        int op, mix1, mix2, mix3, num1, num2, num3, den1, den2, den3;
+        Fraccionario f1, f2 = null, f3, f4, f5 = null, f6, f7 = null, f8=null, f9 = null;
+        
+        op= cmbOperaciones.getSelectedIndex();
+        num1= Integer.parseInt(txtNumerador1.getText());
+        num2= Integer.parseInt(txtNumerador2.getText());
+        
+        
+        den1= Integer.parseInt(txtDenominador1.getText());
+        den2= Integer.parseInt(txtDenominador2.getText());
+        
+        
+        mix1= Integer.parseInt(txtMixto1.getText());
+        mix2= Integer.parseInt(txtMixto2.getText());
+        
+        
+        f1= new Fraccionario (num1, den1, mix1);
+        f4= new Fraccionario (num2, den2, mix2);
+        
+        switch (op)
+        {
+            case 0:
+                f3= f1.convertir(f1);
+                f6= f4.convertir2(f4);
+                f7= f3.suma(f6);
+                f9= f7.convertir3(f7);
+                break;
+            case 1:
+                f3= f1.convertir(f2);
+                f6= f4.convertir2(f4);
+                f7= f3.resta(f6);
+                f9= f7.convertir3(f7);
+                break;
+            case 2:
+                f3= f1.convertir(f1);
+                f6= f4.convertir2(f4);
+                f7= f3.multiplicacion(f6);
+                f9= f7.convertir3(f7);
+                break; 
+            case 3:
+                f3= f1.convertir(f1);
+                f6= f4.convertir2(f4);
+                f7= f3.division(f6);
+                f9= f7.convertir3(f7);
+                break;    
+        }
+        
+        txtNumerador3.setText(""+f9.getNumerador());
+        txtDenominador3.setText(""+f9.getDenominador());
+        txtMixto3.setText(""+f9.getMixto());
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +206,27 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cmbOperaciones;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JSeparator jSeparator4;
+    private javax.swing.JTextField txtDenominador1;
+    private javax.swing.JTextField txtDenominador2;
+    private javax.swing.JTextField txtDenominador3;
+    private javax.swing.JTextField txtDenominador4;
+    private javax.swing.JTextField txtMixto1;
+    private javax.swing.JTextField txtMixto2;
+    private javax.swing.JTextField txtMixto3;
+    private javax.swing.JTextField txtNumerador1;
+    private javax.swing.JTextField txtNumerador2;
+    private javax.swing.JTextField txtNumerador3;
+    private javax.swing.JTextField txtNumerador4;
     // End of variables declaration//GEN-END:variables
 }
